dc7abb35ec0f3a31960a0f335710c6dab171f58e
Ben Forshey <benjamin.a.forshey@nasa.gov>
2025-04-23 10:07:37 -0400

    chore(imports): move imports

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+import { setBasePath } from '@nasa-terra/components'
 import { SelectDateTimeRangeComponent } from './components/select-date-time-range'
 import { SelectSpatialAreaComponent } from './components/select-spatial-area'
 import { SelectVariablesComponent } from './components/select-variables'
 import { SidebarToggleComponent } from './components/sidebar-toggle'
+
+setBasePath('https://cdn.jsdelivr.net/npm/@nasa-terra/components@0.0.28/cdn/')
 
 document.addEventListener('DOMContentLoaded', () => {
     new SidebarToggleComponent('#sidebar-toggle', '#sidebar')
