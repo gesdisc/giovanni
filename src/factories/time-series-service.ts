0ec0fe94f9b9b5e41cedde3d6bb5430c6cf1fced
Jon Carlson <jdcarlson@gmail.com>
2025-07-23 09:14:03 -0400

    chore: remove unused file

--- src/factories/time-series-service.ts
+++
@@ -1,9 +0,0 @@
-import { CloudGiovanniTimeSeriesService } from '../services/cloud-giovanni-time-series'
-import { TimeSeriesService } from '../services/types'
-
-export class TimeSeriesServiceFactory {
-    static getService(): TimeSeriesService {
-        // TODO: logic here to determine which service to use for time series. For now we are just defaulting to Cloud Giovanni
-        return new CloudGiovanniTimeSeriesService()
-    }
-}
